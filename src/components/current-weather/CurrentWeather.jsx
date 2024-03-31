49dddee6ac093c5d2bf1fc079bb93d896cad3f02
DeepakTayde <deepaktayde559@gmail.com>
2024-03-31 15:48:56 +0530

    completed forecast component

--- src/components/current-weather/CurrentWeather.jsx
+++ src/components/current-weather/CurrentWeather.jsx
@@ -2,11 +2,11 @@ import React from "react";
 // import Icons from '../../assets/icons';
 
 const CurrentWeather = ({ data }) => {
-    const imageUrl = `../assets/icons/${data.weather[0].icon}.png}`;
+    // const imageUrl = `../assets/icons/${data.weather[0].icon}.png}`;
 
   return (
     <div className="container">
-      <div className="card center mb-3 p-2" style={{ maxWidth: "420px" }}>
+      <div className="card center mb-3 p-2" style={{ maxWidth: "540px" }}>
         <div className="row g-2 mb-0 d-flex justify-content-between align-items-center">
           <div className="col-md-8">
             <div className="card-body container-fluid">
@@ -15,34 +15,34 @@ const CurrentWeather = ({ data }) => {
             </div>
           </div>
           <div className="col-md-4">
-            <img src={imageUrl} className="img-fluid rounded-start" alt="..." />
+            <img src={`icons/${data.weather[0].icon}.png`} className="img-fluid rounded-start" alt="..." />
           </div>
         </div>
         <div className="row g-0 mt-0 d-flex justify-content-between align-items-center">
           <div className="col-md-7">
-            <h2 className=" text-center container-fluid display-1">18°C</h2>
+            <h2 className=" text-center container-fluid display-1">{Math.round(data.main.temp)}°C</h2>
           </div>
           <div className="col-md-5">
             <h4 className="card-header text-start">Details</h4>
             <div className="card-body">
               <p className="card-text ">
                 <small className="d-flex justify-content-between align-items-center">
-                  Feels Like <span>16°C</span>
+                  Feels Like <span>{Math.round(data.main.feels_like)}°C</span>
                 </small>
               </p>
               <p className="card-text ">
                 <small className="d-flex justify-content-between align-items-center">
-                  Wind <span>2m/s</span>
+                  Wind <span>{data.wind.speed}m/s</span>
                 </small>
               </p>
               <p className="card-text ">
                 <small className="d-flex justify-content-between align-items-center">
-                  Humidity <span>15%</span>
+                  Humidity <span>{data.main.humidity}%</span>
                 </small>
               </p>
               <p className="card-text ">
                 <small className="d-flex justify-content-between align-items-center">
-                  Pressure <span>15hpa</span>
+                  Pressure <span>{data.main.pressure}hPa</span>
                 </small>
               </p>
             </div>
